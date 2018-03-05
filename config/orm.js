a5c70cfa783390bd97764f43b3b17f56d703cb54
randall ishino <rishino24@gmail.com>
2018-03-05 14:07:28 -0800

    server.js comments

---
+++ config/orm.js
@@ -0,0 +1,6 @@
+// importing MySQL connection
+var connection = require("../config/connection.js");
+console.log(connection);
+var orm = {
+
+}
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// requiring packages
 var express = require('express');
 var methodOveride = require('method-override');
 var bodyParser = require('body-parser');
 var app = express();
 var exphbs = require("express-handlebars");
 
+// establishing a port
 var PORT = process.env.PORT  || 3000;
 
 
@@ -14,10 +16,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-
+// using handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// express listener
 app.listen(PORT, function() {
     console.log("App now listening at localhost:" + PORT);
   });
