568974f3d34245982501d1f2cbad50f06a202322
randall ishino <rishino24@gmail.com>
2018-03-12 15:09:53 -0700

    problems with handlebars linking to style.css

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-// requiring packages
+// requiring packages/routes
 var express = require('express');
-var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var app = express();
 var exphbs = require("express-handlebars");
-var routes = require('./controllers/burgers_controller.js');
 var path = require('path');
+var routes = require('./controllers/burgers_controller.js');
+
 
 // establishing a port
 var PORT = process.env.PORT  || 3000;
 
-app.use(express.static(path.join(__dirname, '/public')));
+
+// using handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(express.static(process.cwd() + "/public"));
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,15 +25,9 @@ app.use(methodOverride('_method'));
 
 // parse application/json
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '/public')));
 
-var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
-
-// using handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
 // express listener
 app.listen(PORT, function() {
