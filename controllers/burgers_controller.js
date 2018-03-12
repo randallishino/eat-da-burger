b2ae77c8c314d8a1cdbb5baa2f197b9c174f2bad
randall ishino <rishino24@gmail.com>
2018-03-11 23:51:35 -0700

    burger-controller funcs

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,14 @@ var burger = require('../models/burger.js');
 var server = require('../server.js');
 var router = express.Router();
 console.log(burger);
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var burgerData = {
+            burgers: data
+        };
+        res.render("index", burgerData);
+    });
+  });
+
+  module.exports = router;
