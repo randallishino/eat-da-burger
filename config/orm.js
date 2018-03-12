fd8c51601364a89b224c189b10602e3a7d8765e5
randall ishino <rishino24@gmail.com>
2018-03-11 22:38:10 -0700

    git ignore nodemodules

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,10 @@
 // importing MySQL connection
 var connection = require("../config/connection.js");
 var orm = {
-
+    selectAll: function(input,callback) { 
+        var query = "SELECT * FROM" + input + ";";
+        connection.query(query, function(error,result) {
+            callback(result);
+        });
+    }
 }
